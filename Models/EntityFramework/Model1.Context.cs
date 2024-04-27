aee875558ab6d5a19d297f5e760351a005c0a4e0
Berkay Sanal <150872151+BerkaySanall@users.noreply.github.com>
2024-04-27 21:28:43 +0300

    Add files via upload

---
+++ Models/EntityFramework/Model1.Context.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+namespace MVCProject.Models.EntityFramework
+{
+
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+public partial class Student_DbEntities : DbContext
+{
+    public Student_DbEntities()
+        : base("name=Student_DbEntities")
+    {
+
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        throw new UnintentionalCodeFirstException();
+    }
+
+
+    public virtual DbSet<Tbl_Dersler> Tbl_Dersler { get; set; }
+
+    public virtual DbSet<Tbl_Notlar> Tbl_Notlar { get; set; }
+
+    public virtual DbSet<Tbl_Ogrenci> Tbl_Ogrenci { get; set; }
+
+}
+
+}
+
